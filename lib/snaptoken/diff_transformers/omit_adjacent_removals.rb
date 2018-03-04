65fa80c9ec6d19d3ffa7c462e102c5460763453f
Jeremy Ruten <jeremy.ruten@gmail.com>
2018-03-04 15:43:34 -0600

    OmitAdjacentRemovals diff transformer

--- lib/snaptoken/diff_transformers/omit_adjacent_removals.rb
+++ lib/snaptoken/diff_transformers/omit_adjacent_removals.rb
@@ -1,5 +1,28 @@
 class Snaptoken::DiffTransformers::OmitAdjacentRemovals < Snaptoken::DiffTransformers::BaseTransformer
   def transform(diff)
-    diff.clone
+    new_diff = diff.clone
+
+    removed_lines = []
+    saw_added_line = false
+    new_diff.lines.each.with_index do |line, idx|
+      case line.type
+      when :unchanged, :folded
+        if saw_added_line
+          removed_lines.each do |removed_idx|
+            new_diff.lines[removed_idx] = nil
+          end
+        end
+
+        removed_lines = []
+        saw_added_line = false
+      when :added
+        saw_added_line = true
+      when :removed
+        removed_lines << idx
+      end
+    end
+
+    new_diff.lines.compact!
+    new_diff
   end
 end
