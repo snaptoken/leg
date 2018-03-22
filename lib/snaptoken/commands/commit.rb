8b39dd09151a1a11e4bc65e54466a03f73df0f74
Jeremy Ruten <jeremy.ruten@gmail.com>
2018-03-21 23:51:04 -0600

    Try and get rebasing working (.dummyleg is getting in the way)

--- lib/snaptoken/commands/commit.rb
+++ lib/snaptoken/commands/commit.rb
@@ -17,6 +17,18 @@ class Snaptoken::Commands::Commit < Snaptoken::Commands::BaseCommand
   def run
     needs! :config, :repo
 
+    repo = Rugged::Repository::new(File.join(@tutorial.config[:path], ".leg/repo"))
+    walker = Rugged::Walker.new(repo)
+    walker.sorting(Rugged::SORT_TOPO | Rugged::SORT_REVERSE)
+    master_commit = repo.branches["master"].target
+    if master_commit.oid != repo.head.target_id
+      walker.push(master_commit)
+      walker.hide(repo.head.target)
+      remaining_commits = walker.to_a
+    else
+      remaining_commits = []
+    end
+
     # 1. Copy `step/*` to `.leg/repo/*`
     @tutorial.copy_step_to_repo!
 
@@ -28,10 +40,11 @@ class Snaptoken::Commands::Commit < Snaptoken::Commands::BaseCommand
       `git commit -m"TODO: let user specify commit message"`
 
       # 4. Run `git cherry-pick` on each remaining step, pausing for conflicts
-
+      remaining_commits.each do |commit|
         # 4a. Walk over remaining commits on `master`
 
         # 4b. Run `git cherry-pick` on the current commit hash
+        `git cherry-pick #{commit.oid}`
 
           # 4b(i). If that resulted in a conflict, copy `.leg/repo/*` to `step/*` and
           #        save the commit hash of the step to continue with after the conflict
@@ -40,8 +53,11 @@ class Snaptoken::Commands::Commit < Snaptoken::Commands::BaseCommand
 
           # 4b(ii). Copy `step/*` to `.leg/repo/*`, run `git add -A` and `git commit`
           #         (or however you resolve a cherry-pick conflict...)
+      end
 
       # 5. Update `master` branch to the new rebased branch
+      repo.references.update(repo.branches["master"], repo.head.target_id)
+      repo.head = repo.branches["master"]
     end
   end
 end
